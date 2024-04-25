6e09c6d7c390761f3c2e1bce71c6aeb689668a2f
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2024-04-25 17:49:49 +0400

    fix image creation for layers

--- src/screens/EditorScreen/Layers/Layer/ImageContainer.ts
+++ src/screens/EditorScreen/Layers/Layer/ImageContainer.ts
@@ -20,21 +20,34 @@ function drawPoly(ctx: CanvasRenderingContext2D, x: number, y: number, radius: n
 
 function getImageSrc(mapData: MapData): string {
     const canvas = document.createElement('canvas');
+
+    const imageWidth = 174;
+    const hexWidth = imageWidth / mapData[0].length;
+    const halfHexWidth = hexWidth / 2;
+    const hexRadius = hexWidth / Math.sqrt(3);
+
+    canvas.width = imageWidth + halfHexWidth;
+    canvas.height = 2 * hexRadius * mapData.length;
+
+    if (canvas.width > canvas.height) {
+        canvas.height = canvas.width
+    } else {
+        canvas.width = canvas.height;
+    }
+
     const ctx = canvas.getContext('2d')!;
 
     //ctx.strokeStyle = '#000';
-    //ctx.lineWidth = 1;
+    ctx.lineWidth = 1;
 
     mapData.forEach((row, y) => {
         row.forEach((type: HEX_TYPE, x) => {
             ctx.fillStyle = HEX_CONFIG[type].color;
 
-            const width = 200 / row.length;
-            const radius = width / Math.sqrt(3);
-            const height = 2 * radius;
-            const xOffset = y % 2 === 0 ? width : width / 2;
+            const xOffset = (y % 2 === 0 ? 0 : halfHexWidth) + halfHexWidth;
+            const yOffset = hexRadius;
 
-            drawPoly(ctx, x * width + xOffset, y * 3 * radius / 2 + radius, radius, 6);
+            drawPoly(ctx, x * hexWidth + xOffset, y * 3 * hexRadius / 2 + yOffset, hexRadius, 6);
         })
     })
 
