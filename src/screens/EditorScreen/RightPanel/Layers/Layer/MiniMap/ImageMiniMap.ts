9b936abdd0f271a637a45eaa303c1e2442021de0
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2024-05-06 03:55:59 +0400

    image mini map

--- src/screens/EditorScreen/RightPanel/Layers/Layer/MiniMap/ImageMiniMap.ts
+++ src/screens/EditorScreen/RightPanel/Layers/Layer/MiniMap/ImageMiniMap.ts
@@ -1,5 +1,24 @@
+import { Canvas } from "components/base/Canvas";
 import { observable } from "hoc/observable";
+import { IMAGE_MAP_UPDATE_EVENT } from "screens/EditorScreen/const";
+import { getImageMapData } from "state/imageMapDataActions";
 
-export const ImageMiniMapContainer = observable('', () => {
-    return null
+interface Params {
+    width: number
+    title: string
+}
+
+export const ImageMiniMapContainer = observable(IMAGE_MAP_UPDATE_EVENT, ({width, title}: Params) => {
+    const image = getImageMapData()
+
+    if (!image) return null
+
+    return Canvas(
+        (ctx) => {ctx.drawImage(image, 0, 0, width - 29, width > 170 ? 170 : width)},
+        {
+            title,
+            width: width - 29,
+            height: width > 170 ? 170 : width,
+        }
+    )
 })
--- src/screens/EditorScreen/const.ts
+++ src/screens/EditorScreen/const.ts
@@ -1,5 +1,5 @@
 export const IMAGE_MAP_UPDATE_EVENT = 'image-map-update'
-export const IMAGE_MINI_MAP_UPDATE_EVENT = 'image-mini-map-update'
+//export const IMAGE_MINI_MAP_UPDATE_EVENT = 'image-mini-map-update'
 export const HEX_MAP_UPDATE_EVENT = 'hex-map-update'
 export const HEX_MINI_MAP_UPDATE_EVENT = 'hex-mini-map-update'
 export const LEFT_PANEL_TOGGLE_EVENT = 'toggle-left-panel'
