c35ac3c6d274dad707ac25cf48c6000a8d6cedd7
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2024-11-02 01:12:04 +0400

    ImageMiniMap component

---
+++ src/screens/EditorScreen/RightPanel/Layers/MiniMap/ImageMiniMap.tsx
@@ -0,0 +1,24 @@
+import { Canvas } from 'components/canvas/Canvas';
+
+interface IProps {
+    data: CanvasImageSource;
+    width: number;
+    title: string;
+}
+export function ImageMiniMap({ data, title, ...props }: IProps) {
+    const width = props.width - 29;
+    const height = props.width > 170 ? 170 : props.width;
+
+    return (
+        <Canvas
+            title={title}
+            width={width}
+            height={height}
+            style={{ maxHeight: 170 }}
+        >
+            {(ctx) => {
+                ctx.drawImage(data, 0, 0, width, height);
+            }}
+        </Canvas>
+    );
+}
