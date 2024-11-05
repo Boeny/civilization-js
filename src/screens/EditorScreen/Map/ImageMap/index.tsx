b0d9f15d1ecd1c4cb1f9ae23ce24b36f419acada
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2024-11-05 17:39:47 +0400

    image map scrolling feature

--- src/screens/EditorScreen/Map/ImageMap/index.tsx
+++ src/screens/EditorScreen/Map/ImageMap/index.tsx
@@ -1,12 +1,104 @@
 import './styles.css';
 
+import { memo, useCallback, useEffect, useMemo, useState } from 'react';
+
 import { Canvas } from 'components/canvas/Canvas';
+import { useArrowKeys } from 'hooks/useArrowKeys';
+import { useWheel } from 'hooks/useWheel';
 
 import { IMapProps } from '../types';
 
 import { LoadImageButton } from './LoadImageButton';
 
-export function ImageMap({ isEditable, opacity, width, height, data, zIndex, onDataUpdate }: IMapProps<HTMLImageElement>) {
+interface Point {
+    x: number;
+    y: number;
+}
+
+const SPEED = 5;
+
+interface IProps extends IMapProps<HTMLImageElement> {
+    data: HTMLImageElement;
+}
+const MapComponent = memo(({ opacity, width, height, data, zIndex }: IProps) => {
+    const [isUpPressed, setUpPressed] = useState(false);
+    const [isDownPressed, setDownPressed] = useState(false);
+    const [isLeftPressed, setLeftPressed] = useState(false);
+    const [isRightPressed, setRightPressed] = useState(false);
+    const [position, setPosition] = useState({ x: 0, y: 0 });
+
+    const container = useMemo(() => ({ setClampedPosition: (_x: number, _y: number) => {} }), []);
+
+    const clampPosition = useCallback(
+        (newPosition: Point) => {
+            const deltaWidth = data.width - width;
+            const deltaHeight = data.height - height;
+
+            if (newPosition.x > 0) newPosition.x = 0;
+            if (newPosition.x < -deltaWidth) newPosition.x = -deltaWidth;
+            if (newPosition.y > 0) newPosition.y = 0;
+            if (newPosition.y < -deltaHeight) newPosition.y = -deltaHeight;
+
+            return newPosition;
+        },
+        [data.height, data.width, height, width],
+    );
+
+    container.setClampedPosition = useCallback(
+        (dx: number, dy: number) => {
+            setPosition(clampPosition({ x: position.x + dx, y: position.y + dy }));
+        },
+        [clampPosition, position.x, position.y],
+    );
+
+    useWheel((dx, dy) => container.setClampedPosition(-dx, -dy));
+    useArrowKeys({ setUpPressed, setDownPressed, setLeftPressed, setRightPressed });
+
+    const render = useCallback(() => {
+        let deltaX = 0;
+        let deltaY = 0;
+
+        if (isUpPressed) {
+            deltaY = SPEED;
+        }
+        if (isDownPressed) {
+            deltaY = -SPEED;
+        }
+        if (isLeftPressed) {
+            deltaX = SPEED;
+        }
+        if (isRightPressed) {
+            deltaX = -SPEED;
+        }
+        container.setClampedPosition(deltaX, deltaY);
+    }, [isUpPressed, isDownPressed, isLeftPressed, isRightPressed]);
+
+    useEffect(() => {
+        if (isUpPressed || isDownPressed || isLeftPressed || isRightPressed) requestAnimationFrame(render);
+    }, [position, isDownPressed, isLeftPressed, isRightPressed, isUpPressed]);
+
+    const children = useCallback(
+        (ctx: CanvasRenderingContext2D) => {
+            ctx.drawImage(data, position.x, position.y);
+        },
+        [data, position.x, position.y],
+    );
+
+    return (
+        <Canvas
+            id="image-map"
+            width={width}
+            height={height}
+            style={{ zIndex, opacity }}
+        >
+            {children}
+        </Canvas>
+    );
+});
+
+export function ImageMap(props: IMapProps<HTMLImageElement>) {
+    const { isEditable, opacity, data, zIndex, onDataUpdate } = props;
+
     if (!data) {
         return (
             <div
@@ -22,13 +114,9 @@ export function ImageMap({ isEditable, opacity, width, height, data, zIndex, onD
     }
 
     return (
-        <Canvas
-            id="image-map"
-            width={width}
-            height={height}
-            style={{ zIndex, opacity }}
-        >
-            {(ctx) => ctx.drawImage(data, 0, 0)}
-        </Canvas>
+        <MapComponent
+            {...props}
+            data={data}
+        />
     );
 }
