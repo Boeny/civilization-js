c8bac304fe0925970a54a65aa8e794797f2b2212
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2025-09-29 18:50:59 +0400

    remove useLayerStore, EditorParamsMenuItem

--- src/screens/EditorScreen/layerStore.ts
+++ src/screens/EditorScreen/layerStore.ts
@@ -3,6 +3,6 @@ import { IHexMapParams, LAYER_TYPE } from 'types';
 
 type IStore = { layer: LAYER_TYPE.image } | (IHexMapParams & { layer: LAYER_TYPE.hex });
 
-const [useLayerObservableStore, useLayerStore] = createStoreHook<IStore>({ layer: LAYER_TYPE.image });
+const [useLayerObservableStore] = createStoreHook<IStore>({ layer: LAYER_TYPE.image });
 
-export { useLayerObservableStore, useLayerStore };
+export { useLayerObservableStore };
