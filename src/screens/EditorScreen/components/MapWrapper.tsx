5fe1772c17e6da4f753f678dd34995c6846bff79
aleksandr.sychev <aleksandr.sychev@nordigy.ru>
2025-10-16 01:46:07 +0400

    Map height fix

--- src/screens/EditorScreen/components/MapWrapper.tsx
+++ src/screens/EditorScreen/components/MapWrapper.tsx
@@ -1,6 +1,8 @@
 import { Canvas, CanvasEventHandlers } from 'components/canvas/Canvas';
 import { IPoint } from 'types';
 
+import { TOP_PANEL_HEIGHT } from '../const';
+
 type Props = CanvasEventHandlers & {
     id: string;
     zIndex: number;
@@ -10,16 +12,18 @@ type Props = CanvasEventHandlers & {
 };
 
 export function MapWrapper({ id, zIndex, screenSize, opacity, children, ...props }: Props) {
+    const topPanelHeight = TOP_PANEL_HEIGHT;
+
     return (
         <Canvas
             id={id}
             width={screenSize.x}
-            height={screenSize.y}
+            height={screenSize.y - topPanelHeight}
             style={{ zIndex, opacity }}
             {...props}
         >
             {(ctx) => {
-                ctx.clearRect(0, 0, screenSize.x, screenSize.y);
+                ctx.clearRect(0, 0, screenSize.x, screenSize.y - topPanelHeight);
                 children(ctx);
             }}
         </Canvas>
